3b275d3a8b898de9dc79d9ee5956ab215b5d0a06
Wladyslaw Figura <wfigura@op.pl>
2026-03-14 16:04:02 +0000

    first migration

---
+++ FiguraSp.Users.Solution/FiguraSp.Users.Api/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FiguraSp.Users.Api.Controllers
+{
+    [Route("api/[controller]")] // http://localhost:5000/api/user
+    [ApiController]
+    public class UserController(UserManager<IdentityUser> userManager) : ControllerBase
+    {
+        [HttpGet]
+        [Route("Users")]
+        public async Task<ActionResult> GetUsers()
+        {
+            var users = await userManager.Users.ToListAsync();
+
+            return Ok(users);
+        }
+    }
+}
